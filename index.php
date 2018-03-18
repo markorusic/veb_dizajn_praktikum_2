e3ae017de98ed630ee7497a7ca6d95a949219549
markorusic <markousp5@gmail.com>
2018-03-19 00:32:14 +0100

    footer, btns

--- index.php
+++ index.php
@@ -3,14 +3,22 @@
 		<div class="slider">
 			<div class="overlay"></div>
 			<div class="slider-items">
-				<img src="http://friendsofmedellin.com/wp-content/uploads/2017/08/party.jpg">
+				<img src="./photos/home-1.jpeg">
+			</div>
+			<div class="info flex-center-col">
+				<div class="date">
+					<span class="uc">Wed</span>
+					<span class="uc">24.10</span>
+				</div>
+				<p class="desc text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit consequuntur esse, quibusdam.</p>
+				<a href="#" class="btn-primary">More</a>
 			</div>
 			<div class="options">
 				<a href="#" class="prev btn-dark">
-					prev
+					<i class="fa fa-chevron-left"></i>
 				</a>
 				<a href="#" class="next btn-dark">
-					next
+					<i class="fa fa-chevron-right"></i>
 				</a>
 			</div>
 		</div>
--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,17 @@
-<footer class="main-footer"></footer>
+<footer class="main-footer flex-sp-around">
+	<div class="info text-center">
+		<p class="uc">Belgrade party 2018</p>
+	</div>
+	<div class="links flex-center">
+		<a class="uc" href="#">About us</a>
+		<a class="uc" href="#">Contact</a>
+	</div>
+	<div class="social flex-center">
+		<ul class="flex-list-row">
+			<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+			<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+			<li><a href="#"><i class="fa fa-reddit"></i></a></li>
+		</ul>
+	</div>
+</footer>

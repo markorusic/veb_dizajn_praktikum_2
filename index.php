ac3ce332149504a63b5d9844aecde9531ad2aac7
markorusic <markousp5@gmail.com>
2018-03-18 14:14:19 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include './partials/header.php'; ?>
+	<main id="home">
+		<div class="slider">
+			<div class="overlay"></div>
+			<div class="slider-items">
+				<img src="http://friendsofmedellin.com/wp-content/uploads/2017/08/party.jpg">
+			</div>
+			<div class="options">
+				<a href="#" class="prev btn-dark">
+					prev
+				</a>
+				<a href="#" class="next btn-dark">
+					next
+				</a>
+			</div>
+		</div>
+	</main>
+<?php include './partials/footer.php'; ?>	
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./static/css/main.css">
+	<title>Belgrade party</title>
+</head>
+<body>
+	<header>
+		<nav class="main-nav flex-sp-around">
+			<div class="logo">
+				<a href="/">Belgrade party</a>
+			</div>			
+			<ul class="links flex-list-row">
+				<li><a href="#">Now party</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">News & Events</a></li>
+			</ul>
+			<div class="auth">
+				<ul class="flex-list">
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Register</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>

078d2d8921696cac18af8eb7c299f6f3dde805bb
markorusic <markousp5@gmail.com>
2018-03-22 00:32:43 +0100

    now party page

---
+++ now-party.php
@@ -0,0 +1,7 @@
+<?php include './partials/header.php'; ?>
+	<main id="now-party">
+		<h1>Now party</h1>
+	</main>
+<?php include './partials/footer.php'; ?>	
+</body>
+</html>
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+$('a[href="' + window.location.pathname + '"]').parent().addClass('active');
